39d26ff9083407f23f46ee81f03d4318e547fa0b
Drew Risinger <drewrisinger@users.noreply.github.com>
2021-10-04 13:00:07 -0400

    ci.nix: handle non-set arguments for canEvaluate

--- ci.nix
+++ ci.nix
@@ -17,7 +17,7 @@ let
   isDerivation = p: isAttrs p && p ? type && p.type == "derivation";
   evaluatedPackages = builtins.map builtins.fromJSON (pkgs.lib.splitString "\n" (pkgs.lib.fileContents ./evaluations.json));
 
-  canEvaluate = p: elem: ( (elem ? "attr" && p ? "pname") && (pkgs.lib.hasInfix p.pname elem.attr) && (! (elem ? "error")));
+  canEvaluate = p: elem: ((elem ? "attr" && (if isAttrs p then p ? "pname" else false)) && (pkgs.lib.hasInfix p.pname elem.attr) && (! (elem ? "error")));
   isBuildable = p: any (canEvaluate p) evaluatedPackages;
   isCacheable = p: !(p.preferLocalBuild or false);
   shouldRecurseForDerivations = p: isAttrs p && p.recurseForDerivations or false;
