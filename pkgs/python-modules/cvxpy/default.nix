741a6f0aae59e927cad1a9423ba6f408231c3055
Drew Risinger (bot) <drewrisinger@users.noreply.github.com>
2026-09-06 08:09:17 +0000

    python3Packages.cvxpy: 1.2.1 -> 1.9.2

--- pkgs/python-modules/cvxpy/default.nix
+++ pkgs/python-modules/cvxpy/default.nix
@@ -16,14 +16,14 @@
 
 buildPythonPackage rec {
   pname = "cvxpy";
-  version = "1.2.1";
+  version = "1.9.2";
   format = "pyproject";
 
   disabled = pythonOlder "3.5";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-bWdkJkPR3bLyr1m0Zrh9QsSi42eDGte0PDO1nu+ltQ4=";
+    sha256 = "sha256-suk58ZenCBowDVqVgS/shkP6uvI6FJq/fmfKf4lnHZI=";
   };
 
   propagatedBuildInputs = [
