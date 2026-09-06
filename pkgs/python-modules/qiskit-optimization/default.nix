cfa331d9159cfe08fd9ea59a6533d63d224fca03
Drew Risinger (bot) <drewrisinger@users.noreply.github.com>
2026-09-06 08:10:40 +0000

    python3Packages.qiskit-optimization: 0.3.2 -> 0.7.0

--- pkgs/python-modules/qiskit-optimization/default.nix
+++ pkgs/python-modules/qiskit-optimization/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "qiskit-optimization";
-  version = "0.3.2";
+  version = "0.7.0";
 
   disabled = pythonOlder "3.6";
 
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "Qiskit";
     repo = pname;
     rev = version;
-    sha256 = "sha256-SWrHNHZjynpWcwwrWzRPpbNWz8EhVujMoY8uIJQeT6U=";
+    sha256 = "sha256-aonL08avVZlpGQ/FCZnrsPMvu1lbhRiadzKf/oPndZk=";
   };
 
   propagatedBuildInputs = [
