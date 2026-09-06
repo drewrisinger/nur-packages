af96c9ac2c235567085f742f7a7aa6f1f0321a07
Drew Risinger (bot) <drewrisinger@users.noreply.github.com>
2026-09-06 08:10:29 +0000

    python3Packages.qiskit-ibmq-provider: 0.19.1 -> 0.20.2

--- pkgs/python-modules/qiskit-ibmq-provider/default.nix
+++ pkgs/python-modules/qiskit-ibmq-provider/default.nix
@@ -41,7 +41,7 @@ let
 in
 buildPythonPackage rec {
   pname = "qiskit-ibmq-provider";
-  version = "0.19.1";
+  version = "0.20.2";
 
   disabled = pythonOlder "3.6";
 
@@ -49,7 +49,7 @@ buildPythonPackage rec {
     owner = "Qiskit";
     repo = pname;
     rev = version;
-    sha256 = "sha256-VdGdaOxCwD2Qa0JCCDVZJtcjhmTssS/KgpcjoaPXYB8=";
+    sha256 = "sha256-7dIspeJpukLDfICoBPPZZWdzkVumtvh+NRxvtmnvWH0=";
   };
 
   propagatedBuildInputs = [
